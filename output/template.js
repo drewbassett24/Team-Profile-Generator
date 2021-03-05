bad95413bfb3db2532c1f864e9b01177dc915e09
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-03-05 19:40:44 +0000

    Added a function to create the page (cut and paste from the mock-up.

--- output/template.js
+++ output/template.js
@@ -64,27 +64,64 @@ const genTeam = team => {
     html.push(team
         .filter(employee => employee.getRole() === "Manager")
         .map(manager => generateManager(manager))
-        
+
     );
     html.push(team
         .filter(employee => employee.getRole() === "Engineer")
         .map(engineer => generateEngineer(engineer))
-        
+
     );
     html.push(team
         .filter(employee => employee.getRole() === "Intern")
         .map(intern => generateManager(intern))
         .join("")
-        
+
     );
 
     return html.join("");
 
+}
+
 
 //export this function to generate the page
 module.exports = team => {
-    
-}
+
+return `
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="Width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>TEAM ROSTER</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-2 team-heading">
+                    <h1 style="font-size:80px;" class="text-center">TEAM ROSTER</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                    ${generateTeam(team)}
+                    </div>
+                </div>
+            </div>
+        </body>
+
+        </html>
+    `;
+
+};
 
 
 
